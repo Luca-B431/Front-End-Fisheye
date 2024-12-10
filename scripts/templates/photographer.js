2f14f6932b6b0f7f3fcf44d41f257243513280c7
Luca Billat <lucabillatpro@gmail.com>
2024-12-10 14:17:22 +0100

    CSS adjust & photographerTemplate adjust

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,33 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  //    data =  data.name data.portrait dans la fonction
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const div = document.createElement("div");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    h2.classList.add("light-brown");
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    h3.classList.add("brown");
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    const h5 = document.createElement("h5");
+    h5.textContent = `${price}€/jour`;
+    h5.classList.add("grey");
+    article.appendChild(img);
+    article.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(h5);
+
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
